9735c75898ee932580418ceaa3250f3a08ba88c5
jackh <jvickeryp2223@politecnics.barcelona>
2023-05-25 19:04:29 +0200

    AA

--- src/Map.java
+++ src/Map.java
@@ -1,48 +1,91 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class Map {
-
+    public static int XofclickedLabel;
+    public static int YofclickedLabel;
 
     public static int[][] getMapDataGrass(JPanel placing_panel) {
-        int[][] map = {{1, 1, 1},
-                {1, 1, 1}};
+        int[][] map = new int[30][60];
+
+        for (int col = 0; col < 60; col++) {
+            for (int row = 0; row < 30; row++) {
+                map[row][col] = 1;
+            }
+        }
         return map;
     }
 
-    public static void fillPanelWithJLabels(int[][] mapData, String icon_filePath, JPanel panel) {
+    public static void fillPanelWithJLabels(int[][] mapData, String icon_filePath, JPanel panel, int Tilesize) {
 
         int x = 0;
         int y = 0;
 
 
+        for (int row = 0; row < mapData.length; row++) {
 
+            for (int col = 0; col < mapData[row].length; col++) {
+                if (col == 59) {
+                    y = y + Tilesize;
+                    x = 0;
+                }
+                if (mapData[row][col] == 1 || mapData[row][col] == 2) {
 
-            for (int row = 0; row < mapData.length; row++) {
-                for (int col = 0; col < mapData[row].length; col++) {
-                    if (mapData[row][col] == 1) {
+                    JLabel labelToPlace = new JLabel();
+                    labelToPlace.setSize(new Dimension(Tilesize, Tilesize));
+                    ImageIcon imageCoin = new ImageIcon(icon_filePath);
+                    Icon icon_coin = new ImageIcon(imageCoin.getImage().getScaledInstance(labelToPlace.getWidth(), labelToPlace.getHeight(), Image.SCALE_DEFAULT));
+                    labelToPlace.setIcon(icon_coin);
+                    labelToPlace.setLocation(x, y);
+                    labelToPlace.addMouseListener(new MouseListener() {
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
 
-                        JLabel labelToPlace = new JLabel();
-                        labelToPlace.setSize(new Dimension(20, 20));
-                        ImageIcon imageCoin = new ImageIcon(icon_filePath);
-                        Icon icon_coin = new ImageIcon(imageCoin.getImage().getScaledInstance(labelToPlace.getWidth(), labelToPlace.getHeight(), Image.SCALE_DEFAULT));
-                        labelToPlace.setIcon(icon_coin);
-                        labelToPlace.setLocation(x, y);
+                         if(SwingUtilities.isRightMouseButton(e)) {
+                             ImageIcon imageCoin = new ImageIcon("src/visualResources/grassTileClicked.png");
+                             Icon icon_coin = new ImageIcon(imageCoin.getImage().getScaledInstance(labelToPlace.getWidth(), labelToPlace.getHeight(), Image.SCALE_DEFAULT));
+                             labelToPlace.setIcon(icon_coin);
+                             System.out.println(labelToPlace.getX());
+                         }
+                        }
 
-                        y= y+ 20;
-                        panel.add(labelToPlace);
-                    }
-                    x= x+20;
+                        @Override
+                        public void mousePressed(MouseEvent e) {
+
+                        }
+
+                        @Override
+                        public void mouseReleased(MouseEvent e) {
+
+                        }
+
+                        @Override
+                        public void mouseEntered(MouseEvent e) {
+
+                        }
+
+                        @Override
+                        public void mouseExited(MouseEvent e) {
+
+                        }
+                    });
+
+                    x = x + 20;
+                    panel.add(labelToPlace);
 
                 }
+
             }
 
 
-
-
-
         }
+
     }
+
+    }
+
 
 
 
--- src/panelMain.java
+++ src/panelMain.java
@@ -1,25 +1,26 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class panelMain {
     private JPanel panelMain;
-    private JPanel gameBoard; 
+    private JPanel gameBoard;
     private int[][] mapDataGrass;
+    private int tileSize;
 
 
     public panelMain() {
         panelMain.setPreferredSize(new Dimension(1200, 600));
         panelMain.setLayout(null);
         panelMain.setFocusable(true);
-        System.out.println("width: "+panelMain.getWidth()+ " height: "+ panelMain.getHeight());
+        System.out.println("width: " + panelMain.getWidth() + " height: " + panelMain.getHeight());
         mapDataGrass = Map.getMapDataGrass(panelMain);
-        Map.fillPanelWithJLabels(mapDataGrass, "src/visualResources/grassTile.png", panelMain);
-
-        for (int i = 0; i < mapDataGrass.length ; i++) {
-            for (int j = 0; j < mapDataGrass[i].length ; j++) {
-                System.out.println(mapDataGrass[i][j]);
-            }
-        }
+        tileSize = 20;
+        Map.fillPanelWithJLabels(mapDataGrass, "src/visualResources/grassTile.png", panelMain, tileSize);
+        panelMain.addKeyListener(new panelKeyListener());
+        panelMain.setFocusable(true);
     }
 
     public static void main(String[] args) {
@@ -34,6 +35,21 @@ public class panelMain {
 
 
     }
-
+private class panelKeyListener extends KeyAdapter {
+    public void keyPressed(KeyEvent e){
+        int KeyCode = e.getKeyCode();
+        if(KeyCode == KeyEvent.VK_UP){
+            tileSize+= 10;
+            panelMain.removeAll();
+            Map.fillPanelWithJLabels(mapDataGrass, "src/visualResources/grassTile.png", panelMain, tileSize);
+            panelMain.revalidate();
+            panelMain.repaint();
+            System.out.println("MAS");
+        }
+        if(KeyCode == KeyEvent.VK_DOWN){
+            tileSize-=10;
+        }
+    }
+}
 
 }
